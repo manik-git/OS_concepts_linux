080dd88c8713d03e6131310e7304e91df1c47fa7
manik-git <sharmamanik191@gmail.com>
2024-10-28 12:36:02 +0530

    more comments with better explaination

--- thread/pthread_func.c
+++ thread/pthread_func.c
@@ -15,11 +15,17 @@
     - when a thread function is to exit, this function is called much like exit() is called when a process is ended. 
     - this function terminates the thread. returning a pointer to retval. never return a pointer to a local object as it will cease to exist. 
 
-    int pthread_join(pthread_t *thread, void **thread_retval);
+    int pthread_join(pthread_t thread, void **thread_retval);
     - this is like wait function in process. It is called in main function to wait for a specific thread to be terminated. 
-    - the first argument is pointer to the thread identifier created by pthread_create().
+    - the first argument is pointer to the thread identifier created by pthread_create(). Notice, this is not a pointer.
     - second argument is a double pointer that holds the address of the pointer that points to the object returned by thread. 
+      make a void pointer to a variable and pass its address in second arg, you will only need to deref it once in main() as it would hold the address of the returned object itself.
     - returns 0 on success, non-zero otherwise. 
+    - blocking function
+
+    note: 
+    - creating a new process using fork would have created its own copy of the msg[] and the original one would not have been modified
+    - this code runs 2 threads. one is the main thread and the second one is created using pthread_create()
 
 */
 
@@ -30,6 +36,7 @@
 void *thread_func(void *arg);
 
 char msg[] = "This is a thread program";
+char ret_val[] = "This was a good thread..";
 
 int main()
 {
@@ -53,6 +60,7 @@ int main()
         printf("error joining thread\n");
         exit(-1);
     }
+    printf("value stored in thread_res (in main) : %x\n", thread_res);
 
     printf("\nthread joined.\nThread returned : %s\n", (char *)thread_res);
     printf("msg is now : %s\n", msg);
@@ -65,5 +73,6 @@ void *thread_func(void *arg)
     printf("thread will run for 2 seconds.\narg : %s\n", (char *)arg);
     sleep(2);
     strcpy(msg,"thread complete");
-    pthread_exit("This was a good thread");
+    printf("ret_val addr (in thread) : %x\n", ret_val);
+    pthread_exit((void *)ret_val);
 }
